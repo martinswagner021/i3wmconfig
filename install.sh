8b8a67b98ae2deeec41fc2349a256ca26788904b
Wagner <martinswagner021@gmail.com>
2022-11-11 08:27:13 -0300

    dynamic path with  update

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ echo $(sudo apt install i3 i3blocks fonts-font-awesome xfce4-terminal compton ni
 
 # Copy files into respective folders
 echo $(sudo cp ./bin/i3-sensible-terminal /bin/)
-echo $(cp ./bg.jpg ~/Pictures/)
-echo $(cp ./config/i3/config ~/.config/i3/)
+echo $(cp ./bg.jpg $HOME/Pictures/)
+echo $(cp ./config/i3/config $HOME/.config/i3/)
 echo $(sudo cp ./etc/i3blocks.conf /etc/)
--- save.sh
+++ save.sh
@@ -1,5 +1,5 @@
 # Makes a copy of the current settings in the system
 $(cp /etc/i3blocks.conf ./etc/)
-$(cp ~/.config/i3/config ./config/i3/)
+$(cp $HOME/.config/i3/config ./config/i3/)
 $(cp /bin/i3-sensible-terminal ./bin/)
-$(cp ~/Pictures/bg.jpg ./)
+$(cp $HOME/Pictures/bg.jpg ./)

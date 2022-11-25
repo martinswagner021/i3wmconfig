6e710cb6309b2b5da3d2acfdfb12e8ec401002d5
Wagner <martinswagner021@gmail.com>
2022-11-24 23:19:24 -0300

    Added screenshot capabillities

--- minimalist/install.sh
+++ minimalist/install.sh
@@ -1,6 +1,9 @@
 s="sudo"
 # Install dependencies
-echo $($s apt install i3 i3blocks fonts-font-awesome xfce4-terminal compton nitrogen)
+# Below the functions of each apt to be installed
+# i3*, i3*, icons, compatible terminal, terminal-transparency, wallpaper, screenshots
+
+echo $($s apt install i3 i3blocks fonts-font-awesome xfce4-terminal compton nitrogen flameshot)
 
 # Copy files into respective folders
 echo $($s cp ./bin/i3-sensible-terminal /bin/)
